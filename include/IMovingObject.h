36d8f441f69a12d9ea8ca07c45f4cb3722fce02a
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-28 11:29:00 +0400

    mock для тестирования Move

--- include/IMovingObject.h
+++ include/IMovingObject.h
@@ -3,6 +3,8 @@
 class IMovingObject
 {
 public:
+    virtual ~IMovingObject() = default;
+
     virtual Vector2D getLocation(void) = 0;
     virtual Vector2D getVelocity(void) = 0;
     virtual void setLocation(Vector2D newLocation) = 0;
--- tests/Move_test.cpp
+++ tests/Move_test.cpp
@@ -1,2 +1,16 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include <Move.h>
+
+class IMovingObjectMock: public IMovingObject
+{
+public:
+    MOCK_METHOD(Vector2D, getLocation, (void), (override));
+    MOCK_METHOD(Vector2D, getVelocity, (void), (override));
+    MOCK_METHOD(void, setLocation, (Vector2D newLocation), (override));
+};
+
+TEST(Move, execute)
+{
+    EXPECT_TRUE(true);
+}
