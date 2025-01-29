95acc7e75615e0b45a35ac1594ddc194b4d8849a
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-29 21:34:54 +0400

    Отладка GetLocationFailException

--- tests/Move_test.cpp
+++ tests/Move_test.cpp
@@ -15,8 +15,8 @@ TEST(Move, Moving)
     std::shared_ptr<MovingObjectMock> mo = std::make_shared<MovingObjectMock>();
     Move m(mo);
 
-    EXPECT_CALL(*mo, getLocation()).WillOnce(testing::Return(Vector2D(12,5)));
-    EXPECT_CALL(*mo, getVelocity()).WillOnce(testing::Return(Vector2D(-7,3)));
+    EXPECT_CALL(*mo, getLocation()).WillOnce(testing::Return(Vector2D(12, 5)));
+    EXPECT_CALL(*mo, getVelocity()).WillOnce(testing::Return(Vector2D(-7, 3)));
     EXPECT_CALL(*mo, setLocation(Vector2D(5,8)));
 
     m.Execute();
@@ -27,9 +27,12 @@ TEST(Move, GetLocationFailException)
     std::shared_ptr<MovingObjectMock> mo = std::make_shared<MovingObjectMock>();
     Move m(mo);
 
-    ON_CALL(*mo, getLocation).WillByDefault(testing::Return(Vector2D(12,5)));
-    ON_CALL(*mo, getVelocity).WillByDefault(testing::Return(Vector2D(-7,3)));
-    ON_CALL(*mo, setLocation).WillByDefault(testing::Return());
+    ON_CALL(*mo, getLocation).WillByDefault(testing::Return(Vector2D(12, 5)));
+    ON_CALL(*mo, getVelocity).WillByDefault(testing::Return(Vector2D(-7, 3)));
+
+    EXPECT_CALL(*mo, getLocation()).Times(AnyNamber());
+    EXPECT_CALL(*mo, getVelocity()).Times(AnyNamber());.
+    EXPECT_CALL(*mo, setLocation(Vector2D(testing::_, testing::_))).Times(AnyNamber());
 
     EXPECT_ANY_THROW(m.Execute());
 }
