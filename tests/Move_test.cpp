0c3f70ec7c042807432ca03732cee382c97b4186
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-29 21:04:58 +0400

    Добавлена ошибка в тест Moving и реализован каркас теста GetLocationFailException

--- tests/Move_test.cpp
+++ tests/Move_test.cpp
@@ -10,14 +10,26 @@ public:
     MOCK_METHOD1(setLocation, void(Vector2D));
 };
 
-TEST(Move, execute)
+TEST(Move, Moving)
 {
     std::shared_ptr<MovingObjectMock> mo = std::make_shared<MovingObjectMock>();
     Move m(mo);
 
     EXPECT_CALL(*mo, getLocation()).WillOnce(testing::Return(Vector2D(12,5)));
     EXPECT_CALL(*mo, getVelocity()).WillOnce(testing::Return(Vector2D(-7,3)));
-    EXPECT_CALL(*mo, setLocation(Vector2D(5,8)));
+    EXPECT_CALL(*mo, setLocation(Vector2D(5,9)));
 
     m.Execute();
+}
+
+TEST(Move, GetLocationFailException)
+{
+    std::shared_ptr<MovingObjectMock> mo = std::make_shared<MovingObjectMock>();
+    Move m(mo);
+
+    ON_CALL(*mo, getLocation).WillByDefault(testing::Return(Vector2D(12,5)));
+    ON_CALL(*mo, getVelocity).WillByDefault(testing::Return(Vector2D(-7,3)));
+    ON_CALL(*mo, setLocation);
+
+    EXPECT_ANY_THROW(m.Execute());
 }
