ac714eef9f44e472bd214aba1a1e098cce26c788
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-06 00:21:45 +0400

    Исправление ошибок

--- source/ExceptionHandler.cpp
+++ source/ExceptionHandler.cpp
@@ -1,5 +1,7 @@
 #include <ExceptionHandler.h>
 
+map<pair<string,string>, Handler> ExceptionHandler::Handlers;
+
 ICommandPtr ExceptionHandler::Handle(ICommandPtr pCmd, IExceptionPtr pException)
 {
     Handler handler = Handlers[make_pair(typeid(*pCmd).name(), typeid(*pException).name())];
