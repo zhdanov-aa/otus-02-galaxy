c9cfe29472ffc99ff2267929f61ea2f5d66a1649
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-06 00:02:24 +0400

    Реализован метод

--- include/ExceptionHandler.h
+++ include/ExceptionHandler.h
@@ -3,18 +3,25 @@
 
 #include <ICommand.h>
 #include <IException.h>
+#include <map>
+#include <utility>
+#include <string>
+
+using namespace std;
 
 typedef ICommandPtr(*Handler)(ICommandPtr, IExceptionPtr);
 
 class ExceptionHandler
 {
+    static map<pair<string,string>, Handler> Handlers;
+
 public:
     static ICommandPtr Handle(ICommandPtr pCmd, IExceptionPtr pException);
 
     template<typename CommandType, typename ExceptionType>
     static void Register(Handler handler)
     {
-        //TODO:
+        Handlers[make_pair(typeid(CommandType).name(), typeid(ExceptionType).name())] = handler;
     }
 };
 
