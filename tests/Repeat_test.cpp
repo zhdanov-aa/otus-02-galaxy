5e735d94527c739c0394ef2276f8c40938c995b7
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-08 22:15:52 +0400

    Исправление ошибок

--- tests/Repeat_test.cpp
+++ tests/Repeat_test.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 using ::testing::Return;
+using ::testing::Throw;
 
 TEST(Repeat, Check_success_repeated_command_execute)
 {
@@ -21,7 +22,7 @@ TEST(Repeat, Check_fail_repeated_command_excute)
     shared_ptr<ICommandMock> pCommand = make_shared<ICommandMock>();
     Repeat cmd(pCommand);
 
-    EXPECT_CALL(*pCommand, Execute()).WillOnce(Throw(std::runtime_error("ICommand::Execute() failed")));
+    EXPECT_CALL(*pCommand, Execute()).WillOnce(Throw(runtime_error("ICommand::Execute() failed")));
 
     EXPECT_ANY_THROW(cmd.Execute());
 }
