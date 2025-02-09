f67afede018322b40e5448fa73d29e368f864eaf
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-09 17:54:17 +0400

    Добавлена проверка типа добавляемой команды

--- tests/RepeatHandler_test.cpp
+++ tests/RepeatHandler_test.cpp
@@ -9,13 +9,15 @@ using namespace std;
 using ::testing::Return;
 using ::testing::_;
 
+MATCHER(IsRepeat, "") { return std::dynamic_pointer_cast<Repeat>(arg) != nullptr; }
+
 TEST(RepeatHandler, Check_add_Repeat_command_to_ICommandQueueBuilder)
 {
     shared_ptr<ICommandQueueBuilderMock> pBuilder = make_shared<ICommandQueueBuilderMock>();
     shared_ptr<ICommandMock> pCommand = make_shared<ICommandMock>();
     RepeatHandler cmd(pCommand, pBuilder);
 
-    EXPECT_CALL(*pBuilder, AddCommand(_));
+    EXPECT_CALL(*pBuilder, AddCommand(IsRepeat()));
 
     EXPECT_NO_THROW(cmd.Execute());
 }
