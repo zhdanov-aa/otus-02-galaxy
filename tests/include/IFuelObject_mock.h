23071ed611b85845a4317ab642dbb14d343319ad
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-12 10:30:16 +0400

    Добавлен и реализован mock для IFuelObject

---
+++ tests/include/IFuelObject_mock.h
@@ -0,0 +1,17 @@
+#ifndef __IFUELOBJECT_H_INCLUDED__
+#define __IFUELOBJECT_H_INCLUDED__
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <IFuelObject.h>
+
+class IFuelObjectMock: IFuelObject
+{
+public:
+    MOCK_METHOD(FuelLevel, getLevel, ());
+    MOCK_METHOD(void, setLevel, (FuelLevel));
+
+    MOCK_METHOD(FuelLevel, getConsumption, ());
+};
+
+#endif
