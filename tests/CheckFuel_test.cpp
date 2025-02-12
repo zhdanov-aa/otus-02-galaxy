988808c6ff69422fd983b135b71e16a8b584f399
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-12 18:54:34 +0400

    Исправлены ошибки, добавлен тест CheckFuel.NotEnoughFuel

--- tests/CheckFuel_test.cpp
+++ tests/CheckFuel_test.cpp
@@ -29,17 +29,33 @@ TEST(CheckFuel, EnoughFuel)
     shared_ptr<IFuelObjectMock> obj = make_shared<IFuelObjectMock>();
     CheckFuel cmd(obj);
 
-    EXPECT_CALL(*obj, getFuelLevel()).WillOnce(Return(FuelLevel(100)));
+    EXPECT_CALL(*obj, getLevel()).WillOnce(Return(FuelLevel(100)));
     EXPECT_CALL(*obj, getConsumption()).WillOnce(Return(FuelLevel(40)));
-    EXPECT_CALL(*obj, setFuelLevel(FuelLevel(60)));
+    EXPECT_CALL(*obj, setLevel(FuelLevel(60)));
 
     EXPECT_NO_THROW(cmd.Execute());
 }
 
-// TEST(CheckFuel, NotEnoughFuel)
-// {
-    
-// }
+TEST(CheckFuel, NotEnoughFuel)
+{
+    shared_ptr<IFuelObjectMock> obj = make_shared<IFuelObjectMock>();
+    CheckFuel cmd(obj);
+
+    EXPECT_CALL(*obj, getLevel()).WillOnce(Return(FuelLevel(100)));
+    EXPECT_CALL(*obj, getConsumption()).WillOnce(Return(FuelLevel(40)));
+    EXPECT_CALL(*obj, setLevel(FuelLevel(60)));    
+
+    try
+    {
+        cmd.Execute();
+        FAIL();
+    }
+    catch(IException *exception)
+    {
+        EXPECT_NE(dynamic_cast<NotEnoughFuel*>(exception), nullptr);
+        delete exception;
+    }
+}
 
 // TEST(CheckFuel, TranslateExceptions)
 // {
