26ae8bd848977f8c118a05a3e3fc036211b3d813
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-13 23:09:33 +0400

    Добавлен тест StopOnException для MacroCommand

--- tests/MacroCommand_test.cpp
+++ tests/MacroCommand_test.cpp
@@ -3,6 +3,8 @@
 #include <MacroCommand.h>
 #include <ICommandQueue_mock.h>
 #include <ICommand_mock.h>
+#include <IException_mock.h>
+#include <CommandException.h>
 
 using namespace std;
 using ::testing::Throw;
@@ -34,4 +36,34 @@ TEST(MacroCommand, ExecuteCommands)
     }
     
     EXPECT_NO_THROW(cmd.Execute());
+}
+
+TEST(MacroCommand, StopOnException)
+{
+    shared_ptr<ICommandQueueMock> commands = make_shared<ICommandQueueMock>();
+    shared_ptr<ICommandMock> cmd1 = make_shared<ICommandMock>();
+    shared_ptr<ICommandMock> cmd2 = make_shared<ICommandMock>();
+    MacroCommand cmd(commands);
+
+    EXPECT_CALL(*commands, GetCommand())
+        .WillOnce(Return(cmd1))
+        .WillOnce(Return(cmd2));
+
+    {
+        InSequence s;
+
+        EXPECT_CALL(*cmd1, Execute());
+        EXPECT_CALL(*cmd2, Execute()).WillOnce(Throw new IExceptionMock);
+    }
+    
+    try
+    {
+        cmd.Execute();
+        FAIL();
+    }
+    catch(IException *exception)
+    {
+        EXPECT_NE(dynamic_cast<CommandException*>(exception), nullptr);
+        delete exception;
+    }
 }
