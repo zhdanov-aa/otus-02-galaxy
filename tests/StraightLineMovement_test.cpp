7fa52d5faba11653151e73ad57619e941e5047e5
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-14 23:41:56 +0400

    Исправлены опечатки

--- tests/StraightLineMovement_test.cpp
+++ tests/StraightLineMovement_test.cpp
@@ -16,11 +16,11 @@ using ::testing::AnyNumber;
 
 TEST(StraightLineMovement, MoveAndBurn)
 {
-    shared_ptr<IMovableObjectMock> movableObject = make_shared<IMovableObjectMock>();
+    shared_ptr<IMovingObjectMock> movingObject = make_shared<IMovingObjectMock>();
     shared_ptr<IFuelObjectMock> fuelObject = make_shared<IFuelObjectMock>();
 
     CheckFuelPtr checkFuelCmd = make_shared<CheckFuel>(fuelObject);
-    shared_ptr<Move> moveCmd = make_shared<Move>(movableObject);
+    shared_ptr<Move> moveCmd = make_shared<Move>(movingObject);
     BurnFuelPtr burnFuelCmd = make_shared<BurnFuel>(fuelObject);
 
     shared_ptr<ICommandQueueMock> commands = make_shared<ICommandQueueMock>();
@@ -35,9 +35,9 @@ TEST(StraightLineMovement, MoveAndBurn)
 
     EXPECT_CALL(*fuelObject, getLevel()).WillOnce(Return(FuelLevel(100)));
     EXPECT_CALL(*fuelObject, getConsumption()).WillOnce(Return(FuelLevel(10)));
-    EXPECT_CALL(*movableObject, getLocation()).WillOnce(Return(Vector2D(100,100)));
-    EXPECT_CALL(*movableObject, getVelocity()).WillOnce(Return(Vector2D(2,4)));
-    EXPECT_CALL(*movableObject, setLocation(Vector2D(102,104)));
+    EXPECT_CALL(*movingObject, getLocation()).WillOnce(Return(Vector2D(100,100)));
+    EXPECT_CALL(*movingObject, getVelocity()).WillOnce(Return(Vector2D(2,4)));
+    EXPECT_CALL(*movingObject, setLocation(Vector2D(102,104)));
     EXPECT_CALL(*fuelObject, setLevel(FuelLevel(90)));
 
     EXPECT_NO_THROW(cmd.Execute());
