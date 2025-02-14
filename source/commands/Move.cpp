df164ffab5f725dddbd3544b88c790f3a0a6f333
zhdanov-aa <zhdanov_aa@mail.ru>
2025-02-15 00:05:11 +0400

    Move::Execute() - изменён тип исключения на InvalidArgument

--- source/commands/Move.cpp
+++ source/commands/Move.cpp
@@ -9,12 +9,8 @@ Move::Move(std::shared_ptr<IMovingObject> moveObject)
 
 void Move::Execute()
 {
-    if (m_Object != nullptr)
-    {
-        m_Object->setLocation( m_Object->getLocation() + m_Object->getVelocity() );
-    }
-    else 
-    {
-        throw std::runtime_error("Move::m_Object is nullptr");
-    }
+    if (m_Object == nullptr)
+        throw new InvalidArgument("Move::m_Object is nullptr");
+
+    m_Object->setLocation( m_Object->getLocation() + m_Object->getVelocity() );
 }
