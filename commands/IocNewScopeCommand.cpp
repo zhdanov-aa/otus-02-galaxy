11dec595ebbd1129f08541f1d68d703cb8bcf087
zhdanov-aa <zhdanov_aa@mail.ru>
2025-03-01 13:47:40 +0400

    Добавлена проверка что скопа с именем создаваемого скопа не существует. По результатам выполнения тестов

--- commands/IocNewScopeCommand.cpp
+++ commands/IocNewScopeCommand.cpp
@@ -12,5 +12,8 @@ void IocNewScopeCommand::Execute()
     if (m_pCurrentScope == nullptr)
         throw new RuntimeError("IocNewScopeCommand: m_pCurrentScope is nullptr");
 
+    if (m_pCurrentScope->FindChild(m_sName) != nullptr)
+        throw new RuntimeError(std::string("IocNewScopeCommand: scope with name \"") + m_sName + std::string("\" exist"));
+
     m_pCurrentScope->AddChild( std::make_shared<Scope>(m_sName, m_pCurrentScope) );
 }
