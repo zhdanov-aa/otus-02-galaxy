34b40f2f7c395871396856676387fec5a1096cb7
zhdanov-aa <zhdanov_aa@mail.ru>
2025-03-01 12:29:57 +0400

    Написаны тесты на FallbackCommand

--- tests/FallbackCommand_test.cpp
+++ tests/FallbackCommand_test.cpp
@@ -35,35 +35,26 @@ TEST(FallbackCommand, SkipFailedCommands)
     EXPECT_CALL(*command2, Execute());
     
     EXPECT_NO_THROW(cmd.Execute());
-
-    // shared_ptr<ICommandQueueMock> commands = make_shared<ICommandQueueMock>();
-    // shared_ptr<ICommandMock> cmd1 = make_shared<ICommandMock>();
-    // shared_ptr<ICommandMock> cmd2 = make_shared<ICommandMock>();
-    // MacroCommand cmd(commands);
-
-    // EXPECT_CALL(*commands, GetCommand())
-    //     .WillOnce(Return(cmd1))
-    //     .WillOnce(Return(cmd2));
-
-    // {
-    //     InSequence s;
-
-    //     EXPECT_CALL(*cmd1, Execute());
-    //     EXPECT_CALL(*cmd2, Execute()).WillOnce(Throw(new IExceptionMock));
-    // }
-    
-    // try
-    // {
-    //     cmd.Execute();
-    //     FAIL();
-    // }
-    // catch(IException *exception)
-    // {
-    //     EXPECT_NE(dynamic_cast<CommandException*>(exception), nullptr);
-    //     delete exception;
-    // }
 }
 
-// TEST(FallbackCommand, SkipFailedCommands)
-// {
-// }
+TEST(FallbackCommand, ExceptionOnFailedAllCommands)
+{
+    shared_ptr<ICommandMock> command1 = make_shared<ICommandMock>();
+    shared_ptr<ICommandMock> command2 = make_shared<ICommandMock>();
+    vector<ICommandPtr> commands = { command1, command2 };
+    FallbackCommand cmd(commands);
+
+    EXPECT_CALL(*command1, Execute()).WillOnce(Throw(new IExceptionMock));
+    EXPECT_CALL(*command2, Execute()).WillOnce(Throw(new IExceptionMock));
+
+    try
+    {
+        cmd.Execute();
+        FAIL();
+    }
+    catch(IException *exception)
+    {
+        delete exception;
+        SUCCEED();
+    }
+}
