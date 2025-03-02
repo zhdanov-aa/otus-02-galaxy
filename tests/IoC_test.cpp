f9d0aa74faf71fc0c73015d062de4079b8719bd5
zhdanov-aa <zhdanov_aa@mail.ru>
2025-03-02 17:59:27 +0400

    Добавлен тест RegisterDependency для IoC

---
+++ tests/IoC_test.cpp
@@ -0,0 +1,25 @@
+#include <IoC.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <memory>
+
+using namespace std;
+using ::testing::Throw;
+using ::testing::Return;
+using ::testing::AnyNumber;
+using ::testing::_;
+using ::testing::InSequence;
+
+TEST(IoC, RegisterDependecy)
+{
+    IoC::Resolve<ICommandPtr>(
+        "IoC.Register",
+        "ScopeName",
+        make_container(std::function<string()>( [](){ return string("root"); } ))
+        )->Execute();
+
+    EXPECT_EQ(
+        string("root"),
+        IoC::Resolve<string>("ScopeName")
+    );
+}
