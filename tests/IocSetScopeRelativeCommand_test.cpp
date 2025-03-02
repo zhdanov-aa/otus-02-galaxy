0bf639f875123daa28cd6c364159995fdef15759
zhdanov-aa <zhdanov_aa@mail.ru>
2025-03-02 15:10:38 +0400

    Добавлен тест WrongScopeNameTest

--- tests/IocSetScopeRelativeCommand_test.cpp
+++ tests/IocSetScopeRelativeCommand_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <IocSetScopeRelativeCommand.h>
-//#include <IException.h>
+#include <IException_mock.h>
 #include <IScope_mock.h>
 #include <IScopeChanger_mock.h>
 #include <memory>
@@ -31,4 +31,28 @@ TEST(IocSetScopeRelativeCommand, SunnyDayTest)
     EXPECT_CALL(*scopeChanger, Change(static_pointer_cast<IScope>(targetScope)));
 
     EXPECT_NO_THROW(cmd.Execute());
+}
+
+TEST(IocSetScopeRelativeCommand, WrongScopeNameTest)
+{
+    IScopeMockPtr currentScope = make_shared<IScopeMock>();
+    IScopeMockPtr targetScope = make_shared<IScopeMock>();
+    string targetScopeName = "TestScope";
+    IScopeChangerMockPtr scopeChanger = make_shared<IScopeChangerMock>();
+
+    IocSetScopeRelativeCommand cmd(currentScope, scopeChanger, targetScopeName);
+
+    EXPECT_CALL(*currentScope, FindChild(targetScopeName))
+        .WillOnce(Return(nullptr));
+
+    try
+    {
+        cmd.Execute();
+        FAIL();
+    }
+    catch(IException *exception)
+    {
+        delete exception;
+        SUCCEED();
+    }
 }
