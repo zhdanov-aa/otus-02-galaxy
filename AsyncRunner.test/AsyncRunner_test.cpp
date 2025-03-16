e30d4f6e8f4ccc14dc47caaa1d48d237b2e7f633
zhdanov-aa <zhdanov_aa@mail.ru>
2025-03-16 16:25:19 +0400

    Добавлены HardStopTest и SoftStopTest

--- AsyncRunner.test/AsyncRunner_test.cpp
+++ AsyncRunner.test/AsyncRunner_test.cpp
@@ -3,9 +3,17 @@
 #include <StartCommand.h>
 #include <BlockingQueue.h>
 #include <AsyncRunner.h>
+#include <HardStopCommand.h>
+#include <SoftStopCommand.h>
+#include <ICommand_mock.h>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <memory>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
 
 using namespace std;
 using ::testing::Throw;
@@ -32,4 +40,61 @@ TEST(AsyncRunner, CheckStartCommand)
     EXPECT_NE(r, nullptr);
 
     //TODO: проверить, что поток запущен
+}
+
+TEST(AsyncRunner, HardStopTest)
+{
+    std::mutex              mutex;
+    std::condition_variable condition;
+
+    BlockingQueuePtr q = std::make_shared<BlockingQueue>();
+    AsyncRunnerPtr r = std::make_shared<AsyncRunner>(q);
+
+    std::shared_ptr<ICommandMock> c1 = std::make_shared<ICommandMock>();
+    HardStopCommandPtr hs = std::make_shared<HardStopCommand>(r);
+    std::shared_ptr<ICommandMock> c2 = std::make_shared<ICommandMock>();
+
+    q->Push(c1);
+    q->Push(hs);
+    q->Push(c2);
+
+    r->setStopAction([&condition](){ condition.notify_one(); });
+
+    EXPECT_CALL(*c1, Execute());
+
+    EXPECT_NO_THROW(r->Start());
+
+    std::unique_lock<std::mutex> lock(mutex);
+    condition.wait(lock);
+
+    EXPECT_EQ(1, q->getSize());
+}
+
+TEST(AsyncRunner, SoftStopTest)
+{
+    std::mutex              mutex;
+    std::condition_variable condition;
+
+    BlockingQueuePtr q = std::make_shared<BlockingQueue>();
+    AsyncRunnerPtr r = std::make_shared<AsyncRunner>(q);
+
+    std::shared_ptr<ICommandMock> c1 = std::make_shared<ICommandMock>();
+    SoftStopCommandPtr ss = std::make_shared<SoftStopCommand>(r);
+    std::shared_ptr<ICommandMock> c2 = std::make_shared<ICommandMock>();
+
+    q->Push(c1);
+    q->Push(ss);
+    q->Push(c2);
+
+    r->setStopAction([&condition](){ condition.notify_one(); });
+
+    EXPECT_CALL(*c1, Execute());
+    EXPECT_CALL(*c2, Execute());
+
+    EXPECT_NO_THROW(r->Start());
+
+    std::unique_lock<std::mutex> lock(mutex);
+    condition.wait(lock);
+
+    EXPECT_EQ(0, q->getSize());
 }
